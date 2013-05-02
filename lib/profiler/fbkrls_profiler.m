5633d0f10734a368ee1984a2bdf0449e5fa364f9
Steven <steven2358@gmail.com>
2013-05-02 12:26:12 +0200

    cleanup

--- lib/profiler/fbkrls_profiler.m
+++ lib/profiler/fbkrls_profiler.m
@@ -8,17 +8,6 @@ classdef fbkrls_profiler < fbkrls
     
     methods
         
-        function flops = lastsflops(kaf) % flops for last iteration
-            m = size(kaf.dict,1);
-            if ~kaf.prune, m = m-1; end
-            floptions = struct(...
-                'sum',4*m^2+2 + kaf.prune*(m^2),...
-                'mult',4*m^2+3*m+2 + kaf.prune*(m^2+m),...
-                'div',1 + kaf.prune*(m+2),...
-                'kernel',[kaf.kerneltype,m+1,size(kaf.dict,2)]);
-            flops = kflops(floptions);
-        end
-        
         function flops = lastflops(kaf) % flops for last iteration
             m = size(kaf.dict,1);
             if ~kaf.prune
