64d4f1de2198586fdb447d847131dd60532c8c16
Steven <steven2358@gmail.com>
2013-05-09 19:06:32 +0200

    knlms cc profiler

---
+++ lib/profiler/knlmscc_profiler.m
@@ -0,0 +1,51 @@
+% Profiler extension for Kernel Normalized Least-Mean-Square algorithm with
+% Coherence Criterion
+% Author: Steven Van Vaerenbergh, 2013
+%
+% This file is part of the Kernel Adaptive Filtering Toolbox for Matlab.
+% http://sourceforge.net/projects/kafbox/
+
+classdef knlmscc_profiler < klms
+    
+    methods
+        
+        function flops = lastflops(kaf) % flops for last iteration
+            m = size(kaf.dict,1);
+            if kaf.grow
+                m1 = m - 1;
+                m2 = 1;
+            else
+                m1 = m;
+                m2 = 0;
+            end
+            m3 = m;
+            floptions = struct(...
+                'sum', 3*m3, ...
+                'mult', 3*m3 + 1, ...
+                'kernel', [kaf.kerneltype, m1+m2, size(kaf.dict,2)]);
+            
+            flops = kflops(floptions);
+        end
+        
+        %% flops breakdown
+        
+        % k = kernel(x,kaf.dict,kaf.kerneltype,kaf.kernelpar);
+        % kernel: m1
+        
+        % h = kernel(x,kaf.dict,kaf.kerneltype,kaf.kernelpar);
+        % kernel: m2 % 1 element when growing, 0 when not
+        
+        % kaf.alpha = kaf.alpha + kaf.eta / (kaf.eps + h*h') * (y - h*kaf.alpha) * h';
+        % sum: 3*m3
+        % mult: 3*m3 + 1
+        % div: 1
+        
+        %%
+        
+        function bytes = lastbytes(kaf) % bytes used in last iteration
+            m = size(kaf.dict,1);
+            bytes = 8*(m + m*size(kaf.dict,2)); % 8 bytes for double precision
+        end
+        
+    end
+end
