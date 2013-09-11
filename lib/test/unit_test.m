5544344975f632ca3061c8a1c86e40774f1f5d92
Steven <steven2358@gmail.com>
2013-09-11 18:31:12 +0200

    improve unit test

--- lib/test/unit_test.m
+++ lib/test/unit_test.m
@@ -10,14 +10,17 @@
 
 function unit_test(varargin)
 
-% get list of all algorithms
-files = dir(fullfile('../','*.m'));
-[~,files] = cellfun(@fileparts, {files.name}, 'UniformOutput',false);
-
-% convert into list of algorithms
+% get list of algorithms to run
 if isempty(varargin) || (length(varargin)==1 && strcmp(varargin{1},'all'))
+    pathstr = fileparts(which('unit_test.m'));
+    fdir = [pathstr '/..']; % lib folder
+    files = dir(fullfile(fdir,'*.m'));
+    [~,files] = cellfun(@fileparts, {files.name}, 'UniformOutput',false);
     algorithms = files;
 else
+    fdir = ''; % local folder
+    files = dir(fullfile(fdir,'*.m'));
+    [~,files] = cellfun(@fileparts, {files.name}, 'UniformOutput',false);
     algorithms = lower(varargin);
 end
 
@@ -25,9 +28,10 @@ end
 for a=algorithms,
     unexisting = isempty(find(ismember(files,a{1}),1));
     if unexisting
-        error('Algorithm not found: %s. \nBe sure to run this script from the /test folder.',a{1});
+        error('Algorithm not found: %s.',a{1});
     end
 end
+
 
 % perform test for each specified algorithm
 fprintf('\n')
@@ -35,22 +39,73 @@ for ii=1:length(algorithms)
     algorithm = algorithms{ii};
     
     fprintf('%d. %s:\n',ii,upper(algorithm));
-    fprintf('Constructing object...\n')
-    kaf = feval(algorithm);
-
-    fprintf('Testing training phase')
+    fprintf('Constructing object............\n')
+    try
+        kaf = feval(algorithm);
+    catch err
+        error('Error: %s object cannot be constructed: %s',...
+            upper(algorithm), err.message);
+    end
+    
+    fprintf('Testing arguments..............\n') % bogus arguments
+    opts = struct();
+    try
+        kaf = feval(algorithm, opts);
+    catch err
+        me = err.stack(1);
+        error(['Error in %s constructor: ',...
+            '<a href="matlab: opentoline(which(''%s''),%d)">',...
+            '%s at %d</a>\n',...
+            '%s'],...
+            upper(algorithm), me.file, me.line, me.name, me.line,...
+            err.message);
+    end
+    
+    % test if bogus args are processed
+    opts = struct('shmaloombah',5,'ker',4);
+    try
+        kaf = feval(algorithm, opts);
+    catch err
+        me = err.stack(1);
+        error(['Constructor of %s object is prone ',...
+            'to bogus arguments: ',...
+            '<a href="matlab: opentoline(which(''%s''),%d)">',...
+            '%s at %d</a>\n',...
+            '%s'],...
+            upper(algorithm), me.file, me.line, me.name, me.line,...
+            err.message);
+    end
+    
+    % generate some data
     c = 10;
     N = 10000;
     x = rand(N,2)*c;
     y = sin(3*x(:,1)).*cos(x(:,1)+x(:,2));
-    for i=1:N-1,
+    
+    fprintf('Testing 1 training step........\n')
+    for i=1,
+        try
+            kaf = kaf.train(x(i,:),y(i));
+        catch err
+            me = err.stack(1);
+            error(['Error in %s training: ',...
+                '<a href="matlab: opentoline(which(''%s''),%d)">',...
+                '%s at %d</a>\n',...
+                '%s'],...
+                upper(algorithm), me.file, me.line, me.name, me.line,...
+                err.message);
+        end
+    end
+    
+    fprintf('Testing long training')
+    for i=1:N,
         if ~mod(i,floor(N/10)), fprintf('.'); end
         kaf = kaf.train(x(i,:),y(i));
         % y_test = kaf.evaluate(x(i+1,:));
     end
     fprintf('\n')
-
-    fprintf('Testing evaluation phase...\n')
+    
+    fprintf('Testing evaluation.............\n')
     [x1,x2] = meshgrid(0:.2:c, 0:.2:c);
     yt = kaf.evaluate([x1(:) x2(:)]);
     
