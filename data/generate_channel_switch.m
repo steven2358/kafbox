0e8a95a8233968ebbf863ad09d028a9fa6144df7
Steven <steven2358@gmail.com>
2013-10-10 11:10:58 +0200

    reconvergence demo

---
+++ data/generate_channel_switch.m
@@ -0,0 +1,80 @@
+function [x_embed,y,y_ref,x_test_embed,y_test_ref,H] = generate_channel_switch(opt)
+% Generate CHANNEL_SWITCH data set: input-output data of a nonlinear
+% channel whose linear part is changed abruptly at a chosen point. The
+% nonlinear channel is a Wiener system with a randomly chosen linear part.
+% 
+% Input options: "opt" is a structure with the following fields: 
+%   - N: total number of training data points
+%   - N_switch: iteration after which the channel switch occurs
+%   - N_test: number of test data points (before and after switch)
+%   - chlen: linear channel length
+%   - fun: nonlinear function
+%   - SNR: signal-to-noise ratio of additve output noise
+%
+% Outputs:
+%   - x_embed: system input with time embedding (each datum is a row)
+%   - y: system output
+%   - y_ref: noiseless system output
+%   - x_test_embed: test system input with time embedding
+%   - y_test_ref: noiseless test system output, one column per channel
+%   - H: channel matrix (each row is a channel impulse response)
+%
+% This file is part of the Kernel Adaptive Filtering Toolbox for Matlab.
+% http://sourceforge.net/projects/kafbox/
+
+%% DEFAULT PARAMETER VALUES
+
+options = struct('N',1500,'N_test',500,'N_switch',500,'chlen',5,...
+    'fun','tanh(x)','SNR',20);
+
+%% CUSTOM PARAMETER VALUES
+for opt_name = fieldnames(opt)',
+    if strmatch(opt_name,fieldnames(options),'exact'),
+        options.(opt_name{1}) = opt.(opt_name{1});
+    end
+end
+
+N = options.N;
+N_test = options.N_test;
+N_switch = options.N_switch;
+chlen = options.chlen;
+fun = options.fun;
+SNR = options.SNR;
+
+
+%% PROGRAM
+
+f = inline(fun); % Wiener system nonlinearity
+
+N_all = N+N_test+chlen-1;
+x_all = .5*randn(N_all,1);
+x_all_embed = zeros(N_all,chlen);
+for i = 1:chlen,
+    x_all_embed(i:N_all,i) = x_all(1:N_all-i+1);	% time-embedding, each row is a datum
+end
+x_all_embed = x_all_embed(chlen:N_all,:);
+
+x_embed = x_all_embed(1:N,:);
+x_test_embed = x_all_embed(N+1:N+N_test,:);
+
+H = [ones(2,1) randn(2,chlen-1)]; % linear channel coefficients
+
+% linear filtering
+xp1 = x_embed(1:N_switch,:)*H(1,:)';
+xp2 = x_embed(N_switch+1:N,:)*H(2,:)';
+
+% apply nonlinearity
+y_ref = f([xp1;xp2]); % no noise yet
+
+% add noise
+noisevar = 10^(-SNR/10)*var(y_ref);
+noise = sqrt(noisevar)*randn(N,1);
+
+y = y_ref + noise;
+
+% get test outputs: 2 columns, no noise
+xp3 = x_test_embed*H(1,:)';
+xp4 = x_test_embed*H(2,:)';
+y_test_ref = f([xp3 xp4]);
+
+% figure;plot([xp1;xp2],y,'.')
---
+++ demo/demo_reconverge_all.m
@@ -0,0 +1,87 @@
+% Demo: learn a nonlinear channel with an abrupt change. Run and compare
+% all algorithms using their default parameters.
+%
+% This file is part of the Kernel Adaptive Filtering Toolbox for Matlab.
+% http://sourceforge.net/projects/kafbox/
+
+close all
+clear all
+% rs = 1; randn('state',rs); rand('state',rs); %#ok<RAND>
+
+%% PARAMETERS
+
+N = 1500; % number of training data
+N_switch = 500; % iteration after which the channel switch occurs
+N_test = 100; % number of test data
+SNR = 30; % SNR in dB
+
+%% PROGRAM
+
+w = who;
+for i=1:length(w), % copy all parameters to option structure
+    eval(sprintf('opt.%s = %s;',w{i},w{i}))
+end
+
+% generate data
+[X,y,y_ref,X_test,y_test] = generate_channel_switch(opt);
+
+% get list of kernel adaptive filters in 'lib' folder
+fdir = fileparts(which('kafbox_template.m'));
+files = dir(fullfile(fdir,'*.m'));
+[~,algorithms] = cellfun(@fileparts, {files.name}, 'UniformOutput',0);
+for i=length(algorithms):-1:1,
+    if ~exist(algorithms{i},'class'),
+        algorithms(i) = []; % remove files that do not represent classes
+    end
+end
+
+% algorithms = {'krlst'};
+
+% perform online learning for each algorithm
+fprintf('\n')
+num_alg = length(algorithms);
+titles = cell(num_alg,1);
+MSE = zeros(N,num_alg);
+MSE_avg = zeros(1,num_alg);
+for algo_ind=1:num_alg
+    t1 = tic;
+    algorithm = algorithms{algo_ind};
+    fprintf('%2d. %9s: ',algo_ind,upper(algorithm));
+    titles{algo_ind} = strrep(upper(algorithm),'_','\_');
+
+    kaf = feval(algorithm);
+    for i=1:N,
+        if ~mod(i,floor(N/10)), fprintf('.'); end
+        
+        y_est = kaf.evaluate(X_test);
+		if i<=N_switch
+			MSE(i,algo_ind) = mean((y_test(:,1)-y_est).^2);
+		else
+			MSE(i,algo_ind) = mean((y_test(:,2)-y_est).^2);
+		end			
+        
+        kaf = kaf.train(X(i,:),y(i));
+    end
+    MSE_avg(algo_ind) = mean(MSE(:,algo_ind));
+    
+    fprintf(' %.2fs. Average MSE=%3.2fdB\n',toc(t1),10*log10(MSE_avg(algo_ind)))
+end
+
+%% OUTPUT
+
+% plot results in different "leagues"
+[MSE_avg_sorted,ind] = sort(MSE_avg,'descend');
+num_fig = ceil(num_alg/5);
+counter = [rem(num_alg,5) rem(num_alg,5)+1];
+for fig_ind=num_fig:-1:1
+    figure; hold all
+    for i=1:counter(1),
+        counter(2) = counter(2) - 1;
+        plot(10*log10(MSE(:,ind(counter(2)))),'LineWidth',1)
+    end
+    title(sprintf('League %d',fig_ind))
+    legend(titles(ind(counter(2)+counter(1)-1:-1:counter(2))))
+    counter(2) = counter(2)+counter(1)+5;
+    counter(1) = 5;
+    axis([0 N 5*floor(min(10*log10(MSE(:)))/5) 0]);
+end
