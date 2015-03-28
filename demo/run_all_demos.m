db6e110ed45c350cd317c10a5f3247bd4712b5ca
Steven <steven2358@gmail.com>
2015-03-28 23:01:49 +0100

    add script to run all demos

---
+++ demo/run_all_demos.m
@@ -0,0 +1,33 @@
+% Script to run all demos consecutively.
+%
+% Author: Steven Van Vaerenbergh (steven *at* gtas.dicom.unican.es), 2015.
+%
+% This file is part of the Kernel Adaptive Filtering Toolbox for Matlab.
+% https://github.com/steven2358/kafbox
+
+clear
+close all
+
+% get list of test functions
+fdir = fileparts(which('run_all_demos.m'));
+files = dir(fullfile(fdir,'demo_*.m'));
+[~,allfiles] = cellfun(@fileparts, {files.name}, 'UniformOutput',0);
+
+t1 = tic;
+fprintf('\n')
+for i=1:length(allfiles)
+    close all
+    clear eval
+    save('run_temp','i','allfiles','t1');
+    
+    % run script
+    fname_demo = allfiles{i};
+    fprintf('\nRunning %s\n',fname_demo);
+    eval(fname_demo);
+    
+    load run_temp
+end
+delete run_temp.mat
+toc(t1)
+
+close all
