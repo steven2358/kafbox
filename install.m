99f15ab728d561a071a518ea21487cf775c6ab55
Steven <steven2358@gmail.com>
2015-09-02 15:48:18 +0200

    clean ups

--- install.m
+++ install.m
@@ -2,10 +2,10 @@
 
 fprintf('Adding KAFBOX folders to Matlab path... ')
 
-addpath(genpath(fullfile(pwd,'data')));
-addpath(genpath(fullfile(pwd,'lib')));
+addpath(genpath(fullfile(pwd,'data'))); % add data folder with subfolders
+addpath(genpath(fullfile(pwd,'lib'))); % add lib folder with subfolders
 
-addpath((fullfile(pwd,'demo')));
+addpath(fullfile(pwd,'demo')); % add demo folder without subfolders
 
 fprintf('done.\n')
 disp('Type "savepath" if you wish to store the changes.')
