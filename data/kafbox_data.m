2ed94bead9cd166fc6b3b1fb62be6726195eb238
Steven Van Vaerenbergh <steven2358@gmail.com>
2016-10-26 09:46:56 +0200

    improve kafbox_data

--- data/kafbox_data.m
+++ data/kafbox_data.m
@@ -4,7 +4,7 @@
 % This file is part of the Kernel Adaptive Filtering Toolbox for Matlab.
 % https://github.com/steven2358/kafbox/
 
-function [X,Y,X_test,Y_test] = kafbox_data(data_options)
+function [X_train,Y_train,X_test,Y_test] = kafbox_data(data_options)
 
 if isfield(data_options,'class')
     data_name = lower(data_options.class);
@@ -19,8 +19,10 @@ dataset_handle = str2func(['kafbox_data_' data_name]);
 [X,Y,X_test,Y_test] = feval(dataset_handle, data_options);
 
 % time embedding
+L = 0;
 if isfield(data_options,'embedding')
-    X = time_embedding(X,data_options.embedding);
+    L = data_options.embedding;
+    X = time_embedding(X,L);
 end
 
 % apply offset
@@ -34,13 +36,22 @@ N = length(Y);
 if isfield(data_options,'N')
     N = min(data_options.N,N);
 end
-X = X(1:N,:);
-Y = Y(1:N);
+X_train = X(1:N,:);
+Y_train = Y(1:N);
+
+if isfield(data_options,'n_train') && isfield(data_options,'n_test')
+    n_train = data_options.n_train;
+    n_test = data_options.n_test;
+    X_train = X(L:n_train+L-1,:);
+    Y_train = Y(L:n_train+L-1);
+    X_test = X(n_train+L:n_train+L+n_test-1,:);
+    Y_test = Y(n_train+L:n_train+L+n_test-1);
+end
 
 
 function X_embedded = time_embedding(X,L)
 N = size(X,1);
-X_embedded = zeros(N,L);
+X_embedded = zeros(N,L); % L-1 first vectors are zero padded
 for i = 1:L,
     X_embedded(i:N,i) = X(1:N-i+1,1); % time embedding
 end
