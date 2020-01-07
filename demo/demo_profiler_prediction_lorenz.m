bf91ba5fc3f2a54f495be167872bd7dbb7cdf322
Steven Van Vaerenbergh <steven2358@gmail.com>
2020-01-07 17:51:07 +0100

    bug fix (for new matlab)

--- demo/demo_profiler_prediction_lorenz.m
+++ demo/demo_profiler_prediction_lorenz.m
@@ -68,7 +68,7 @@ fprintf('Running profiler for %d algorithms on %s data.\n',i,data.name);
 %     output_dir = output_dir_default;
 %     fprintf('Using default folder "%s" for storing results.\n', output_dir_default);
 % end
-output_dir = fullfile(mfilename('fullpath'),'..','results');
+output_dir = fullfile(fileparts(mfilename('fullpath')),'results');
 
 t1 = tic;
 [data,algorithms,results] = kafbox_profiler(data,sim_opts,algorithms,output_dir);
