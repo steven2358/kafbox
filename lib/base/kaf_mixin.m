893b70330ad714f3af7e5fbed0f439fa5434ad55
Steven Van Vaerenbergh <steven2358@gmail.com>
2021-05-12 12:09:26 +0200

    add kaf_mixin

---
+++ lib/base/kaf_mixin.m
@@ -0,0 +1,8 @@
+% Mixin class for kernel adaptive filters
+%
+% This file is part of the Kernel Adaptive Filtering Toolbox for Matlab.
+% https://github.com/steven2358/kafbox/
+
+classdef kaf_mixin < matlab.mixin.Copyable
+    % Use a superclass that provides copy functionality for handle objects.
+end
